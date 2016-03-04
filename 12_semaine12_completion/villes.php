4c7d788353a7b50b90163285fad61796ba161e51
titzelle <pseudoups@hotmail.fr>
2016-03-04 15:29:32 +0100

    modification de l'arborescence et ajout semaine 12 autocompletion

---
+++ 12_semaine12_completion/villes.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emmanuelle
+ * Date: 26/02/2016
+ * Time: 18:02
+ */
+$file    = file_get_contents( "towns.txt" );
+$villes  = unserialize( $file );
+$results = [ ];
+$text    = "Aucune ville en correspond à votre recherche";
+
+if ( isset ( $_POST['findme'] ) && $_POST['findme'] != '' ) {
+	$findme = $_POST['findme'];
+
+	foreach ( $villes as $ville ) {
+		$pos = stripos( $ville, $findme );
+		if ( $pos === 0 ) {
+			array_push( $results, $ville );
+		}
+		sort( $results );
+		$text = implode( '|', $results );
+	}
+}
+echo $text;
