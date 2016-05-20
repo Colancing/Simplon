8d5fca16ba9c2f32ff4b662b121b3c05f03284ff
titzelle <pseudoups@hotmail.fr>
2016-05-21 01:17:21 +0200

    quizz fini. Reste à analyser les reponses

--- 13QuizzAngular/App/app.js
+++ 13QuizzAngular/App/app.js
@@ -6,50 +6,63 @@
 var quizzApp = angular.module("quizzApp", []);
 
 quizzApp.controller("questionController", function ($scope, Questions) {
-    $scope.questions = Questions;
-    $scope.activeQuestion = 0;
-    $scope.error = false;
+        $scope.questions = Questions;
+        $scope.activeQuestion = 0;
+        $scope.error = false;
+        $scope.finalise = false;
 
-    var numQuestionsAnswered = 0;
+        var numQuestionsAnswered = 0;
 
 
-    $scope.setActiveQuestion = function (index) {
-        if (index != undefined) {
-            $scope.activeQuestion = index;
-        }
-        else {
-            var breakOut = false;
-            while (!breakOut) {
-                $scope.activeQuestion = $scope.activeQuestion < $scope.questions.length - 1
-                    ?
-                    ++$scope.activeQuestion : 0;
-                if ($scope.activeQuestion ===0){
-                    $scope.error = true;
-                }
-                if ($scope.questions[$scope.activeQuestion].selected === null) {
-                    breakOut = true;
+        $scope.setActiveQuestion = function (index) {
+            if (index != undefined) {
+                $scope.activeQuestion = index;
+            }
+            else {
+                var breakOut = false;
+                var length = $scope.questions.length - 1;
+                while (!breakOut) {
+                    if ($scope.activeQuestion < length) {
+                        $scope.activeQuestion++;
+                    } else {
+                        $scope.activeQuestion = 0;
+                        $scope.error = true;
+                    }
+                    if ($scope.questions[$scope.activeQuestion].selected === null) {
+                        breakOut = true;
+                    }
                 }
             }
-        }
-    };
+        };
 
-    $scope.nextQuestion = function () {
-        if ($scope.questions[$scope.activeQuestion].selected !== null) {
-            numQuestionsAnswered++;
-            console.log('nombre de questions répondues passe à', numQuestionsAnswered);
-            if (numQuestionsAnswered >= $scope.questions.length) {
-                console.log("quiz fini, place au calcul")
+        $scope.nextQuestion = function () {
+            if ($scope.questions[$scope.activeQuestion].selected !== null) {
+                numQuestionsAnswered++;
+                console.log('answered', numQuestionsAnswered);
+                console.log('sur/', $scope.questions.length);
+                if (numQuestionsAnswered >= $scope.questions.length) {
+                    for (var i = 0; i < $scope.questions.length; i++) {
+                        if ($scope.questions[i].selected === null) {
+                            $scope.setActiveQuestion(i);
+                            return;
+                        }
+                    }
+                    $scope.error = false;
+                    $scope.finalise = true;
+                    console.log('Voici les résultats !');
+                    return;
+
+                }
+                else {
+                    $scope.setActiveQuestion();
+                }
             }
-            else console.log('la question est selectionnée, on peut calculer la prochaine question');
-        }
-        $scope.setActiveQuestion();
-    };
+        };
 
-    $scope.selectedPossibility = function (index) {
-        $scope.questions[$scope.activeQuestion].selected = index;
-
-        //    passer les trois autres sur false, colorer la poss selectionnnée
-    };
+        $scope.selectedPossibility = function (index) {
+            $scope.questions[$scope.activeQuestion].selected = index;
+        };
 
 
-});
+    }
+);
